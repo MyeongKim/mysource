415eac29889047339767adeee45c76f5ce7dd752
mingkim <ming3772@gmail.com>
2014-07-20 18:56:44 +0900

    jhjh

--- crawlingfromboard.py
+++ crawlingfromboard.py
@@ -1,14 +1,25 @@
-import urllib
+import urllib2
 from bs4 import BeautifulSoup
 
-htmltext = urllib.urlopen("http://valley.egloos.com/theme/food").read()
+# 
 
-soup = BeautifulSoup(htmltext, from_encoding="utf-8")
 
-authors = []
+for i in range(1,10):
+	temp_url = "http://valley.egloos.com/theme/food/page/"
+	url = temp_url + str(i)
+	user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+	headers = { 'User-Agent' : user_agent }
+	r = urllib2.Request(url, headers=headers)
 
-for tag in soup.select(".popular"):
-	authors.append(tag.title)
+	htmltext = urllib2.urlopen(r,timeout=10).read()
+	soup = BeautifulSoup(htmltext, from_encoding="utf-8")
+#
+	authors = []
 
-for author in authors:
-	print author.encode('utf-8')
+	for tag in soup.select(".popular"):
+		authors.append(tag.get_text())
+
+	for author in authors:
+		print author.encode('utf-8');
+
+
--- mytest2.py
+++ mytest2.py
@@ -5,18 +5,18 @@ import urllib2
 import json
 
 
+for i in range(1,500):
+	temp_url = "http://map.naver.com/search2/local.nhn?query=%EC%B2%AD%EB%8B%B4%EB%8F%99+%EC%8A%A4%EC%8B%9C&page="
+	url = temp_url + str(i)
+	user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+	headers = { 'User-Agent' : user_agent }
+	r = urllib2.Request(url, headers=headers)
 
-temp_url = "http://map.naver.com/search2/local.nhn?query=%EC%B2%AD%EB%8B%B4%EB%8F%99+%EC%8A%A4%EC%8B%9C&page=2"
-url = temp_url
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-headers = { 'User-Agent' : user_agent }
-r = urllib2.Request(url, headers=headers)
+	htmltext = urllib2.urlopen(r,timeout=10).read()
+	data = json.loads(htmltext)
 
-htmltext = urllib2.urlopen(r,timeout=10).read()
-data = json.loads(htmltext)
-
-for item in data['result']['site']['list'] :
-	print item['name'].encode('utf-8');
-	print item['roadAddress'].encode('utf-8');
-	print item['streetPanorama']['lng'].encode('utf-8');
-	print item['streetPanorama']['lat'].encode('utf-8');
+	for item in data['result']['site']['list'] :
+		print item['name'].encode('utf-8');
+		print item['roadAddress'].encode('utf-8');
+		print item['streetPanorama']['lng'].encode('utf-8');
+		print item['streetPanorama']['lat'].encode('utf-8');
